90f368ab3efd798692ebebdd05ce763a071a1e38
coocie <577144911@qq.com>
2020-05-28 14:24:53 +0800

    Update common.php

--- common.php
+++ common.php
@@ -40,7 +40,34 @@ function curl_request($url, $isPost = FALSE, $postData = NULL, $useCert = FALSE,
     return $result;
 }
 
-
+/*
+*以raw格式传数据，java接口
+*/
+function request_by_curl($remote_server, $post_string)
+{
+    $curl = curl_init();
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => $remote_server,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => $post_string,
+        CURLOPT_HTTPHEADER => array(
+            "Content-Type: application/json",
+            "cache-control: no-cache"
+        ),
+    ));
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
+    if ($err) {
+        return "cURL Error #:" . $err;
+    } else {
+        return $response;
+    }
 
 /*
  * 确定时间与当前时间对比
